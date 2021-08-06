e11043822cd5f456a7470587c05ab0d9f4669cd7
Ramon Metzker <ramonmetzker12@gmail.com>
2021-08-06 10:19:55 -0400

    final app

--- src/Tmdb.js
+++ src/Tmdb.js
@@ -1,4 +1,3 @@
-const API_KEY = "0ac44df180bbf4ef07c1a8fe451d13ef";
 const API_BASE_URL = "https://api.themoviedb.org/3";
 
 /*
@@ -26,56 +25,56 @@ export default {
         slug: "originals",
         title: "Originais do Netflix",
         items: await basicFetch(
-          `/discover/tv?with_networks=213&language=pt-BR&api_key=${API_KEY}`
+          `/discover/tv?with_networks=213&language=pt-BR&api_key=${process.env.REACT_APP_TMDB_KEY}`
         ),
       },
       {
         slug: "trending",
         title: "Recomendados para Você",
         items: await basicFetch(
-          `/discover/movie?sort_by=vote_count.desc&language=pt-BR&api_key=${API_KEY}`
+          `/discover/movie?sort_by=vote_count.desc&language=pt-BR&api_key=${process.env.REACT_APP_TMDB_KEY}`
         ),
       },
       {
         slug: "toprated",
         title: "Em Alta",
         items: await basicFetch(
-          `/movie/top_rated?language=pt-BR&region=br&api_key=${API_KEY}`
+          `/movie/top_rated?language=pt-BR&region=br&api_key=${process.env.REACT_APP_TMDB_KEY}`
         ),
       },
       {
         slug: "action",
         title: "Ação",
         items: await basicFetch(
-          `/discover/movie?with_genres=28&language=pt-BR&api_key=${API_KEY}`
+          `/discover/movie?with_genres=28&language=pt-BR&api_key=${process.env.REACT_APP_TMDB_KEY}`
         ),
       },
       {
         slug: "comedy",
         title: "Comédia",
         items: await basicFetch(
-          `/discover/movie?with_genres=35&language=pt-BR&api_key=${API_KEY}`
+          `/discover/movie?with_genres=35&language=pt-BR&api_key=${process.env.REACT_APP_TMDB_KEY}`
         ),
       },
       {
         slug: "horror",
         title: "Terror",
         items: await basicFetch(
-          `/discover/movie?with_genres=27&language=pt-BR&api_key=${API_KEY}`
+          `/discover/movie?with_genres=27&language=pt-BR&api_key=${process.env.REACT_APP_TMDB_KEY}`
         ),
       },
       {
         slug: "romance",
         title: "Romance",
         items: await basicFetch(
-          `/discover/movie?with_genres=10749&language=pt-BR&api_key=${API_KEY}`
+          `/discover/movie?with_genres=10749&language=pt-BR&api_key=${process.env.REACT_APP_TMDB_KEY}`
         ),
       },
       {
         slug: "documentary",
         title: "Documentários",
         items: await basicFetch(
-          `/discover/movie?with_genres=99&language=pt-BR&api_key=${API_KEY}`
+          `/discover/movie?with_genres=99&language=pt-BR&api_key=${process.env.REACT_APP_TMDB_KEY}`
         ),
       },
     ];
@@ -87,12 +86,12 @@ export default {
       switch (type) {
         case "movie":
           info = await basicFetch(
-            `/movie/${movieId}?language=pt-BR&api_key=${API_KEY}`
+            `/movie/${movieId}?language=pt-BR&api_key=${process.env.REACT_APP_TMDB_KEY}`
           );
           break;
         case "tv":
           info = await basicFetch(
-            `/tv/${movieId}?language=pt-BR&api_key=${API_KEY}`
+            `/tv/${movieId}?language=pt-BR&api_key=${process.env.REACT_APP_TMDB_KEY}`
           );
           break;
         default:
--- src/components/FeaturedMovie.js
+++ src/components/FeaturedMovie.js
@@ -9,6 +9,11 @@ export default ({ item }) => {
     genres.push(item.genres[i].name);
   }
 
+  let description = item.overview;
+  if (description.length > 200) {
+    description = description.substring(0, 200) + "...";
+  }
+
   return (
     <section
       className="featured"
@@ -28,7 +33,7 @@ export default ({ item }) => {
               {item.number_of_seasons} temporada
               {item.number_of_seasons !== 1 ? "s" : ""}
             </div>
-            <div className="featured--description">{item.overview}</div>
+            <div className="featured--description">{description}</div>
             <div className="featured--buttons">
               <a className="featured--watchbutton" href={`/watch/${item.id}`}>
                 ▶ Assistir
